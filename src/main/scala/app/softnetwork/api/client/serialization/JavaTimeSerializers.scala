51cc45a370a5226d8a597b88dc2ea671ad878d00
Stéphane Manciot <stephane.manciot@gmail.com>
2021-04-09 11:00:27 +0200

    Update JavaTimeSerializers.scala

--- src/main/scala/app/softnetwork/api/client/serialization/JavaTimeSerializers.scala
+++ src/main/scala/app/softnetwork/api/client/serialization/JavaTimeSerializers.scala
@@ -9,9 +9,10 @@ import scala.util.Try
 
 /**
   * @author Valentin Kasas
+  * @author smanciot
   */
 object JavaTimeSerializers {
-  def all = Seq(LocalDateTimeISOSerializer, LocalDateISOSerializer)
+  def all = Seq(LocalDateTimeISOSerializer, LocalDateISOSerializer, ZonedDateTimeISOSerializer)
 }
 
 case object LocalDateTimeISOSerializer
@@ -42,6 +43,22 @@ case object LocalDateISOSerializer
           jt.LocalDate.parse(value, isoFormat)
       }, {
       case ldt: jt.LocalDate => JString(ldt.format(isoFormat))
+    }
+      )
+  })
+
+case object ZonedDateTimeISOSerializer
+  extends CustomSerializer[jt.ZonedDateTime](format => {
+
+    val isoFormat                             = jt.format.DateTimeFormatter.ISO_DATE_TIME
+    def isValidDateTime(str: String): Boolean = Try(isoFormat.parse(str)).isSuccess
+
+    (
+      {
+        case JString(value) if isValidDateTime(value) =>
+          jt.ZonedDateTime.parse(value, isoFormat)
+      }, {
+      case zdt: jt.ZonedDateTime => JString(zdt.format(isoFormat))
     }
       )
   })
